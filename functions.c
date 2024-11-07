472a49ded1703d595b13f2b011ab8d48fb8e4e73
ayaan shaikh <107512637+ayaannshaikhh@users.noreply.github.com>
2024-11-07 10:55:43 -0500

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,28 +1,3 @@
-/************************shaikhAyaanA2.c**************
-Student Name: Ayaan Shaikh         Email Id: ashaikh26
-Due Date: November 8th, 2024       Course Name: CIS*1300
-I have exclusive control over this submission via my password.
-By including this statement in this header comment, I certify that:
-1) I have read and understood the University policy on academic integrity. 2) I
-have completed the Computing with Integrity Tutorial on Moodle; and 3) I have
-achieved at least 80% in the Computing with Integrity Self Test.
-I assert that this work is my own. I have appropriately acknowledged any and
-all material that I have used, whether directly quoted or paraphrased.
-Furthermore, I certify that this assignment was prepared by me specifically for
-this course.
-********************************************************/
-
-/*********************************************************
-Compiling the program
-The program should be compiled using the following flags: -std=c99 -Wall
-compiling:
-gcc -std=c99 -Wall shaikhAyaanA2.c shaikhAyaanA2Main.c
-Running: ./a.out
-OR
-gcc -std=c99 -Wall shaikhAyaanA2.c shaikhAyaanA2Main.c -o assn2
-Running the Program: ./assn2
-*********************************************************/
-
 /*
 Citations
 sscanf() was used with help of this website:

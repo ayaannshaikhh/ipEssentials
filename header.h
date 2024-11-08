dc993d2a914b561259100abe01a96d48d6655d66
Ayaan Shaikh <ayaannshaikhh8@gmail.com>
2024-11-08 11:39:31 -0500

    Removed unneccessary comments.

--- header.h
+++ header.h
@@ -3,16 +3,11 @@
 
 #define MAX 16
 
-// Task 1
 void readIPAddress (char ipAddress []);
 
-// Task 2
 long int convertIPToLongNumber ( char ipAddress [], int lengthIPAddr, int * numDigits);
 
-// Task 3
 char classifyIPAddress (char ipAddress []);
-
-// helper functions
 
 int countDig (int);
 void convertToBinary (int octet, int octetBinary [8]);
